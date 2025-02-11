e329fac73ce1a50c7718af52783d170267bc4350
PaulaEsteve <paulaeg78@gmail.com>
2025-02-11 15:36:32 +0100

    creacio components formularis

---
+++ src/app/documentos/documento-create-form/documento-create-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documento-create-form',
+  imports: [],
+  templateUrl: './documento-create-form.component.html',
+  styleUrl: './documento-create-form.component.css'
+})
+export class DocumentoCreateFormComponent {
+
+}
---
+++ src/app/documentos/documento-edit-form/documento-edit-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documento-edit-form',
+  imports: [],
+  templateUrl: './documento-edit-form.component.html',
+  styleUrl: './documento-edit-form.component.css'
+})
+export class DocumentoEditFormComponent {
+
+}
