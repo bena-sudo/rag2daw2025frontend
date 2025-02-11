cb77550738773834f93115e9400419fb670a3aaf
PaulaEsteve <paulaeg78@gmail.com>
2025-02-11 19:38:28 +0100

    correcciones css

--- src/app/documentos/main-documento/main-documento.component.ts
+++ src/app/documentos/main-documento/main-documento.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-main-documento',
-  imports: [],
+  imports: [ ReactiveFormsModule, CommonModule],
   templateUrl: './main-documento.component.html',
   styleUrl: './main-documento.component.css'
 })
-export class MainDocumentoComponent {
+export class MainDocumentoComponent  implements OnInit{
+
+  searchForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.searchForm = this.formBuilder.group({
+      searchInput: [''],
+    });
+  }
+
+  ngOnInit() : void {
+    this.searchForm.get('searchInput')?.valueChanges
+    .pipe(
+      debounceTime(1000)  // Espera 1 segundo tras el último cambio
+    )
+    .subscribe();
+
+  }
 
 }
