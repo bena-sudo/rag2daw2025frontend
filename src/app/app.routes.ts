99a95a2cc7b6d691ab99b3c926a75dc0c3b5184e
PaulaEsteve <paulaeg78@gmail.com>
2025-02-12 18:36:21 +0100

    v1 prueba chartsjs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainDocumentoComponent } from './documentos/main-documento/main-documento.component';
+import { GraficasEstadisticasComponent } from './components/graficas-estadisticas/graficas-estadisticas.component';
 
 export const routes: Routes = [
 
-    {path: '', component: MainDocumentoComponent}
+    {path: 'mainDocument', component: MainDocumentoComponent},
+
+    {path: 'grafica', component: GraficasEstadisticasComponent}
 ];
