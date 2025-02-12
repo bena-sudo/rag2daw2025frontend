04f218e055935975c4cb757e2c061452c6168468
Laura11mp <laura_mico_perez@hotmail.com>
2025-02-12 15:44:00 +0100

    interficie formulari edició document

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DocumentoEditFormComponent } from './documentos/documento-edit-form/documento-edit-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'formEdit', component: DocumentoEditFormComponent}
+    
+];
