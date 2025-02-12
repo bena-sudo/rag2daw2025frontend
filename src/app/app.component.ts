140da622d858dab33632b706f937cf84d517c0d8
bena-sudo <joanbenaventcastells@gmail.com>
2025-02-12 18:55:00 +0100

    Modificar app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./componentes_global/footer/footer.component";
 import { HeaderComponent } from "./componentes_global/header/header.component";
-import { EtiquetaGridComponent } from "./etiqueta/etiqueta-grid/etiqueta-grid.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent,  EtiquetaGridComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
