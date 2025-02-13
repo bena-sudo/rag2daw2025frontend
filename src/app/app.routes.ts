ae70a3190b7bc91a5ab404d8020470909a450b42
PaulaEsteve <paulaeg78@gmail.com>
2025-02-13 15:57:06 +0100

    createForm proves

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DocumentoCreateFormComponent } from './documentos/documento-create-form/documento-create-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'createForm', component: DocumentoCreateFormComponent}
+];
