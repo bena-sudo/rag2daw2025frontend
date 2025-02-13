9557404f49d6b44107425f39a63ceea964e7ee1e
matecijas <matecijas@gmail.com>
2025-02-13 18:06:34 +0100

    intento de stats

--- src/app/componentes_calidad/estadisticas/filtros/filtros.component.ts
+++ src/app/componentes_calidad/estadisticas/filtros/filtros.component.ts
@@ -1,11 +1,74 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../../../service/service';
+import { EnviarFitrosService } from '../../chat/enviar-fitros.service';
 
 @Component({
   selector: 'app-filtros',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './filtros.component.html',
   styleUrl: './filtros.component.css'
 })
 export class FiltrosComponent {
+
+  selectedGroup: string = "";
+
+  apply: any;
+
+  users: string[] = [];
+  chunks: string[] = [];
+
+  filtros = {
+    filterUser: "",
+    filterPregunta: "",
+    filterRespuesta: "",
+    filterChunks: "",
+    filterValorado: "",
+    filterFeedback: "",
+  }
+
+  fechaInicio: string = "";
+  fechaFin: string = "";
+
+  constructor(
+    private apiService: ApiService,
+    private enviarFiltrosService: EnviarFitrosService
+  ) { };
+
+  ngOnInit() {
+    this.iniciarListaNombres();
+    this.apply = document.getElementById("apply-filters");
+  }
+
+
+  iniciarListaNombres() {
+    this.apiService.getListUsuarios().subscribe(
+      list => this.users = list,
+      error => console.error("Error al conseguir los usuarios: ", error)
+    );
+  }
+
+
+  bodyFiltros: { [key: string]: any } = {};
+  aplicarEstadisticas() {
+    for (let [key, value] of Object.entries(this.filtros))
+      if (value != "")
+        this.bodyFiltros[key] = value;
+
+    let stringFecha = `${this.fechaInicio != "" ? this.fechaInicio : "null"},${this.fechaFin != "" ? this.fechaFin : "null"}`;
+
+    if (stringFecha != 'null,null')
+      this.bodyFiltros['filterRango'] = stringFecha;
+
+
+    let agrupacion = ""
+    if (this.selectedGroup != "") {
+        agrupacion= "?groupBy="+this.selectedGroup
+    }
+
+
+    this.apiService.getStats({"body":this.bodyFiltros, agrupacion}).subscribe( data => console.log(data));
+  }
 
 }
--- src/app/componentes_calidad/estadisticas/grafica/grafica.component.ts
+++ src/app/componentes_calidad/estadisticas/grafica/grafica.component.ts
@@ -1,4 +1,6 @@
 import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { ApiService } from '../../../service/service';
+import { EnviarFitrosService } from '../../chat/enviar-fitros.service';
 //import Chart from 'chart.js/auto';
 
 @Component({
@@ -9,6 +11,46 @@ import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 })
 export class GraficaComponent/* implements AfterViewInit*/ {
   @ViewChild('myChart') chartRef!: ElementRef;
+  selectedGroup: string = "";
+
+	constructor(
+		private apiService: ApiService,
+		private enviarFiltrosService: EnviarFitrosService 
+	) {};
+
+
+  ngOnInit() {
+		this.suscripcionStats();
+	}
+
+	suscripcionStats() {
+		this.enviarFiltrosService.stats$.subscribe( objeto => {
+			this.apiService.getStats(objeto).subscribe( 
+				page => {
+
+          console.log(page);
+          
+/*
+
+					this.chartRef.nativeElement, {
+            type: 'pie',
+            data: {
+              labels: ['BOT', 'Denis', 'Mateo', 'Jack'],
+              datasets: [{
+                label: 'Media de carácteres',
+                data: [25, 10, 9, 5],
+                backgroundColor: ['red', 'blue', 'yellow', 'green', 'purple', 'orange']
+              }]
+            }
+          };*/
+				
+				},
+				error => console.error("Error al conseguir los usuarios: ", error)
+			  );
+		})
+	}
+
+
 /*
   ngAfterViewInit() {
     new Chart(this.chartRef.nativeElement, {
