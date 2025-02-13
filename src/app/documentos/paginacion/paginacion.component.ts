966225df4d08bc33ae73a077f06d18ef6ac01af0
IvanTormo <ivatormar3@alu.edu.gva.es>
2025-02-13 20:37:16 +0100

    Paginación finalizada

--- src/app/documentos/paginacion/paginacion.component.ts
+++ src/app/documentos/paginacion/paginacion.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-paginacion',
@@ -7,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './paginacion.component.css'
 })
 export class PaginacionComponent {
+  @Input() paginas!: number;
+  pagina: number = 1;
 
+  @Output() eventoHijo = new EventEmitter<number>();
+
+  notificarPadre(valor: number) {
+    this.eventoHijo.emit(valor);
+  }
+
+  modificarPagina(simbolo: string){
+    let valor = this.pagina;
+    if (simbolo === '+') {
+      valor++;
+    } else if (simbolo === '-') {
+      valor--;
+    }
+    this.validarValor(valor);
+  }
+
+  private validarValor(valor: number){
+    if ( valor < 1 || valor > this.paginas ) {
+      return false;
+    }
+    this.pagina = valor;
+    this.notificarPadre(valor);
+    return true;
+  }
+
+  importarValue(event:Event){
+    const input = event.target as HTMLInputElement;
+    if (!this.validarValor(Number.parseInt(input.value))) {
+      input.value = this.pagina+"";
+    }
+  }
 }
