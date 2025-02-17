88bec853ab42a81a829967334aa4bdaa9df092c1
bena-sudo <joanbenaventcastells@gmail.com>
2025-02-17 18:11:58 +0100

    Crear vista detalle documento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { ChunksComponent } from './chunks/chunks/chunks.component';
 import { EtiquetaGridComponent } from './etiqueta/etiqueta-grid/etiqueta-grid.component';
 import { MainDocumentoComponent } from './documentos/main-documento/main-documento.component';
+import { DocumentoDetailComponent } from './documentos/documento-detail/documento-detail.component';
 
 export const routes: Routes = [
-
-    {path: '', component: MainDocumentoComponent},
-    {path: 'chunks', component: ChunksComponent},
-    {path: 'etiqueta', component: EtiquetaGridComponent},
+  { path: '', component: MainDocumentoComponent },
+  { path: 'chunks', component: ChunksComponent },
+  { path: 'etiqueta', component: EtiquetaGridComponent },
+  { path: 'documento/:id', component: DocumentoDetailComponent },
 ];
