ae50fc000def06b7e6ac016e09c77c3de66a25a1
Quico <blesapenadesquico@gmail.com>
2025-02-20 20:06:32 +0100

    Correccio de errors1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,2 +1,6 @@
 //Rutas para produccion
-export const environment = {};
+export const environment = {
+    production: false,
+    authUrl: 'http://localhost:8090/auth',
+    apiUrl: 'http://localhost:8090/api'
+};
