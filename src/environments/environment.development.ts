795398ee747cdc7e6fd1e2f14b89eef0b4bebe80
Quico <blesapenadesquico@gmail.com>
2025-02-20 20:16:07 +0100

    Despliege completo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 //Rutas para trabajar en deploiment
 export const environment = {
     production: false,
-    authUrl: 'http://13.73226.200:8090/auth',
-    apiUrl: 'http://13.73226.200:8090/api'
+    authUrl: 'http://13.73.226.200:8090/auth',
+    apiUrl: 'http://13.73.226.200:8090/api'
 };
